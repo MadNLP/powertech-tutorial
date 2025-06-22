f00bd3e40e23fa5b18da634ea315f9a55e15839f
fpacaud <francoispacaud8@gmail.com>
2025-06-22 16:34:56 +0200

    generate jupyter notebooks

--- make.jl
+++ make.jl
@@ -7,3 +7,10 @@ Literate.markdown("1-powerflow.jl", dest_dir)
 Literate.markdown("2-block-powerflow.jl", dest_dir)
 Literate.markdown("3-constrained-powerflow.jl", dest_dir)
 Literate.markdown("4-optimal-powerflow.jl", dest_dir)
+
+dest_dir = "notebooks"
+Literate.notebook("0-crashcourse.jl", dest_dir)
+Literate.notebook("1-powerflow.jl", dest_dir)
+Literate.notebook("2-block-powerflow.jl", dest_dir)
+Literate.notebook("3-constrained-powerflow.jl", dest_dir)
+Literate.notebook("4-optimal-powerflow.jl", dest_dir)
