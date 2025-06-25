0ac41cc9690fc4080961f9f0d147643c5f35a63b
Sungho Shin <sshin@anl.gov>
2025-06-25 13:49:06 -0400

    0,1 pass

--- make.jl
+++ make.jl
@@ -1,8 +1,9 @@
 using Literate
 
 dest_dir = "docs/src"
-cp("utils.jl", dest_dir; force=true)
-cp("powerflow.jl", dest_dir; force=true)
+for script in ["utils.jl", "powerflow.jl"]
+    cp(script, joinpath(dest_dir, script); force=true)
+end
 Literate.markdown("0-crashcourse.jl", dest_dir)
 Literate.markdown("1-powerflow.jl", dest_dir)
 Literate.markdown("2-block-powerflow.jl", dest_dir)
@@ -10,8 +11,9 @@ Literate.markdown("3-constrained-powerflow.jl", dest_dir)
 Literate.markdown("4-optimal-powerflow.jl", dest_dir)
 
 dest_dir = "notebooks"
-cp("utils.jl", dest_dir; force=true)
-cp("powerflow.jl", dest_dir; force=true)
+for script in ["utils.jl", "powerflow.jl"]
+    cp(script, joinpath(dest_dir, script); force=true)
+end
 Literate.notebook("0-crashcourse.jl", dest_dir)
 Literate.notebook("1-powerflow.jl", dest_dir)
 Literate.notebook("2-block-powerflow.jl", dest_dir)
